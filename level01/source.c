6eff041f7308f72e10971b3a2b1f14371c0f1d36
adacher <adr.dacher@gmail.com>
2020-06-18 02:25:01 +0200

    00/01 -> english

--- level01/source.c
+++ level01/source.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-int verify_user_name(char *name) {
+int verify_user_name(char *name)
+{
     int test_name;
 
     puts("verifying username....\n");
@@ -10,31 +11,35 @@ int verify_user_name(char *name) {
     return test_name;
 }
 
-int verify_user_pass(char *pass) {
+int verify_user_pass(char *pass)
+{
     int test_pass;
 
     test_pass = strncmp(pass, "admin", 5);
     return test_pass;
 }
 
-int main () {
+int main()
+{
     int verify_name;
     int verify_pass;
-    char name[100];
-    char pass[80];
+    char name[256];
+    char pass[16];
 
     puts("********* ADMIN LOGIN PROMPT *********");
     printf("Enter Username:");
     fgets(name, 256, stdin);
     verify_name = verify_user_name(name);
-    if (verify_name != 0) {
+    if (verify_name != 0)
+    {
         puts("nope, incorrect username...\n");
         return 0;
     }
     puts("Enter Password\n");
     fgets(pass, 100, stdin);
     verify_pass = verify_user_pass(pass);
-    if (verify_pass || !verify_pass) {
+    if (verify_pass || !verify_pass)
+    {
         puts("nope, incorrect password...\n");
         return 0;
     }
